521063157a2cd89c1f1a0f1454c0c862b67a998a
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-05-02 14:40:29 -0300

    inital commit

---
+++ src/test/kotlin/printscript/group13/snippetmanager/SnippetManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package printscript.group13.snippetmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
