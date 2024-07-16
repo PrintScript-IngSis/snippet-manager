58a9b4f48f3d6abc2aa2bc1cfa389a6fb758cd4a
Lourdes Pontiroli <pontiroli.lourdes@gmail.com>
2024-07-16 16:58:22 -0300

    fix: linter build(build.gradle): added ktlint and kover

--- src/main/kotlin/printscript/group13/snippetmanager/permissions/model/SnippetPermission.kt
+++ src/main/kotlin/printscript/group13/snippetmanager/permissions/model/SnippetPermission.kt
@@ -2,7 +2,7 @@ package printscript.group13.snippetmanager.permissions.model
 
 import java.util.UUID
 
-data class SnippetPermission(override val permission: String, val snippetId: UUID, val userId: String, val username: String): Permission {
+data class SnippetPermission(override val permission: String, val snippetId: UUID, val userId: String, val username: String) : Permission {
     override val assetId: UUID
         get() = snippetId
 }
