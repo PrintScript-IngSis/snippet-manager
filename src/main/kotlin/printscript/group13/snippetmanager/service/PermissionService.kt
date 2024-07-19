3b2172cad02a2cb424541b4b145008b3a6b94fd5
Lourdes Pontiroli <pontiroli.lourdes@gmail.com>
2024-07-19 04:48:04 -0300

    feat(permission service): new relic loggers delete(permission service): getPermissionsByUserId BREAKING CHANGE

--- src/main/kotlin/printscript/group13/snippetmanager/service/PermissionService.kt
+++ src/main/kotlin/printscript/group13/snippetmanager/service/PermissionService.kt
@@ -1,6 +1,7 @@
 package printscript.group13.snippetmanager.service
 
 import PermissionTypeDTO
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -18,8 +19,10 @@ class PermissionService(
     @Value("http://snippet-permission-app:8081/api/permissions") private val url: String,
     private val restTemp: RestTemplate,
 ) {
+    private val logger = LoggerFactory.getLogger(PermissionService::class.java)
     fun createPermission(permissionDTO: PermissionDTO): ResponseEntity<Permission> {
         val completeUrl = "$url/${permissionDTO.snippetId}/users/${permissionDTO.userId}"
+        logger.info("Creating permission for ${permissionDTO.userId}")
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         val permissionDTOType = PermissionTypeDTO(permissionDTO.permission)
@@ -27,6 +30,9 @@ class PermissionService(
 
         val response = restTemp.postForEntity(completeUrl, requestEntity, Permission::class.java)
         val createdPermission = response.body
+        if (createdPermission != null) {
+            logger.info("Permission created for userId ${createdPermission.userId} and snippet id ${createdPermission.snippetId}")
+        }
         return ResponseEntity(createdPermission, response.statusCode)
     }
 
@@ -35,32 +41,30 @@ class PermissionService(
         snippetId: UUID,
     ): ResponseEntity<PermissionDTO> {
         val completeUrl = "$url/$snippetId/users/$userId"
+        logger.info("Getting user permissions for userId $userId")
         val response = restTemp.getForEntity(completeUrl, PermissionDTO::class.java)
         val permissions = response.body
+        logger.info("Permissions found for userId $userId")
         return ResponseEntity(permissions, response.statusCode)
     }
 
     fun updatePermission(permissionDTO: PermissionDTO): ResponseEntity<Permission> {
         val completeUrl = "$url/api/permissions/${permissionDTO.snippetId}/users/${permissionDTO.userId}"
+        logger.info("Updating permission for ${permissionDTO.userId}")
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         val permissionDTOType = PermissionTypeDTO(permissionDTO.permission)
         val requestEntity = HttpEntity(permissionDTOType, headers)
-
         val response = restTemp.exchange(completeUrl, HttpMethod.PATCH, requestEntity, Permission::class.java)
         return ResponseEntity(response.body, response.statusCode)
     }
 
     fun deletePermission(snippetId: UUID): ResponseEntity<Unit> {
         val completeUrl = "$url/$snippetId"
+        logger.info("Deleting user permissions for snippetId $snippetId")
         restTemp.delete(completeUrl)
+        logger.info("User deleted for snippetId $snippetId")
         return ResponseEntity.noContent().build()
     }
 
-    fun getPermissionsByUserId(userId: String): ResponseEntity<List<Permission>> {
-        val completeUrl = "$url/user/$userId"
-        val response = restTemp.getForEntity(completeUrl, Array<Permission>::class.java)
-        val permissions = response.body?.toList()
-        return ResponseEntity(permissions, response.statusCode)
-    }
 }
