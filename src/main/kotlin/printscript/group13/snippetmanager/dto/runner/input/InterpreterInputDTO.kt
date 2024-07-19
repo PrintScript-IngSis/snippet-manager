b854b4acae1d89a0bc2cc8144e1f5d984b36d5f2
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2024-07-19 04:38:44 -0300

    fix: interpreter mocked input is only one

--- src/main/kotlin/printscript/group13/snippetmanager/dto/runner/input/InterpreterInputDTO.kt
+++ src/main/kotlin/printscript/group13/snippetmanager/dto/runner/input/InterpreterInputDTO.kt
@@ -3,6 +3,6 @@ package printscript.group13.snippetmanager.dto.runner.input
 data class InterpreterInputDTO(
     val code: String,
     val version: String? = null,
-    val inputs: List<String>? = null,
+    val input: String? = null,
     val env: Map<String, Any>? = null
 )
