378a01119734fe81f5dd69544c97e60ca0df06eb
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2024-07-19 00:42:57 -0300

    update and delete permissions and get permissions by userid

--- src/main/kotlin/printscript/group13/snippetmanager/service/PermissionService.kt
+++ src/main/kotlin/printscript/group13/snippetmanager/service/PermissionService.kt
@@ -2,10 +2,7 @@ package printscript.group13.snippetmanager.service
 
 import PermissionTypeDTO
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import printscript.group13.snippetmanager.dto.Permission
@@ -36,6 +33,30 @@ class PermissionService(
         val completeUrl = "$url/$snippetId/users/$userId"
         val response = restTemp.getForEntity(completeUrl, PermissionDTO::class.java)
         val permissions = response.body
+        return ResponseEntity(permissions, response.statusCode)
+    }
+
+    fun updatePermission(permissionDTO: PermissionDTO): ResponseEntity<Permission> {
+        val completeUrl = "$url/api/permissions/${permissionDTO.snippetId}/users/${permissionDTO.userId}"
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val permissionDTOType = PermissionTypeDTO(permissionDTO.permission)
+        val requestEntity = HttpEntity(permissionDTOType, headers)
+
+        val response = restTemp.exchange(completeUrl, HttpMethod.PATCH , requestEntity, Permission::class.java)
+        return ResponseEntity(response.body, response.statusCode)
+    }
+
+    fun deletePermission(snippetId: UUID): ResponseEntity<Unit> {
+        val completeUrl = "$url/$snippetId"
+        restTemp.delete(completeUrl)
+        return ResponseEntity.noContent().build()
+    }
+
+    fun getPermissionsByUserId(userId: String): ResponseEntity<List<Permission>> {
+        val completeUrl = "$url/user/$userId"
+        val response = restTemp.getForEntity(completeUrl, Array<Permission>::class.java)
+        val permissions = response.body?.toList()
         return ResponseEntity(permissions, response.statusCode)
     }
 }
